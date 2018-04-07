87d19e65af8d79d47d5dd57b1bfaa3dd22e586d9
Interferon16 <36331860+Interferon16@users.noreply.github.com>
2018-04-07 21:25:50 +0300

    add task1

--- Module2/src/task1/NewArrayList.java
+++ Module2/src/task1/NewArrayList.java
@@ -1,18 +1,70 @@
 package task1;
 
-import java.util.ArrayList;
 
-public class NewArrayList<E>{
-    private E[] array;
+public class NewArrayList<E> {
+    private int length;
+    private Object[] array;
+    private int size = 0;
 
-    public void add(E element){
+    public NewArrayList() {
+        this(10);
+    }
 
+    public NewArrayList(int length) {
+        this.array = new Object[length];
+        this.length = length;
+    }
+
+    public int addElement(E element) {
+        growArray();
+        array[size++] = element;
+        return size;
+    }
+
+    public void add(E element, int index) {
+        growArray();
+        if (size <= index) {
+            addElement(element);
+        } else {
+            for (int i = size; i >= index; i--) {
+                array[size] = array[size - 1];
+            }
+            array[index] = element;
+            size++;
+        }
+    }
+
+    public E get(int index) {
+        return (E) array[index];
     }
 
 
-    ArrayList list = new ArrayList();
+    public E remove(int index) {
+        E result = (E) array[index];
+        array[index] = null;
+        for (int i = index; i < size; i++) {
+            array[index] = array[index + 1];
+        }
+        array[size - 1] = null;
+        return result;
+    }
 
-    private void somm(){
-        list.
+    private void growArray() {
+        if (size > array.length * 0.6) {
+            int new_size = (int) (array.length * 1.5);
+            Object[] new_array = new Object[new_size];
+            System.arraycopy(array, 0, new_array, 0, array.length);
+            array = new_array;
+            this.length = array.length;
+        }
+    }
+
+
+    public int size() {
+        return size;
+    }
+
+    public int length() {
+        return length;
     }
 }
