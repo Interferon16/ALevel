b392ed331f70b440e53c8540714c9e2ebd49088e
Interferon16 <36331860+Interferon16@users.noreply.github.com>
2018-04-07 19:30:05 +0300

    Add files via upload
    module 2 via browser

---
+++ Module2/src/Main.java
@@ -0,0 +1,24 @@
+import task2.PingPong;
+import task3.DBGenerate;
+import task3.Workbench;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task number");
+        Scanner scanner = new Scanner(System.in);
+        int task_num=scanner.nextInt();
+        switch(task_num){
+            case 2:
+                PingPong.run();break;
+            case 3:
+                System.out.println("Enter path to Class.csv");
+                String path = scanner.next().replace("\\","\\\\")
+                                            .replace("/","\\\\");
+                new DBGenerate(path).run();break;
+            case 4:
+                break;
+        }
+    }
+}
---
+++ Module2/src/task1/NewArrayList.java
@@ -0,0 +1,18 @@
+package task1;
+
+import java.util.ArrayList;
+
+public class NewArrayList<E>{
+    private E[] array;
+
+    public void add(E element){
+
+    }
+
+
+    ArrayList list = new ArrayList();
+
+    private void somm(){
+        list.
+    }
+}
