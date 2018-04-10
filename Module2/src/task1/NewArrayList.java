7e0354b4ce40804c6c12e98d2af00bdd4dd37c76
Interferon16 <36331860+Interferon16@users.noreply.github.com>
2018-04-11 01:53:01 +0300

    task1 fix

--- Module2/src/task1/NewArrayList.java
+++ Module2/src/task1/NewArrayList.java
@@ -1,8 +1,12 @@
 package task1;
 
 
-public class NewArrayList<E> {
-    private int length;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class NewArrayList<E> implements List {
     private Object[] array;
     private int size = 0;
 
@@ -10,15 +14,14 @@ public class NewArrayList<E> {
         this(10);
     }
 
-    public NewArrayList(int length) {
-        this.array = new Object[length];
-        this.length = length;
+    public NewArrayList(int start_length) {
+        this.array = new Object[start_length];
     }
 
     public int addElement(E element) {
         growArray();
         array[size++] = element;
-        return size;
+        return size-1;
     }
 
     public void add(E element, int index) {
@@ -38,6 +41,16 @@ public class NewArrayList<E> {
         return (E) array[index];
     }
 
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
 
     public E remove(int index) {
         E result = (E) array[index];
@@ -49,13 +62,37 @@ public class NewArrayList<E> {
         return result;
     }
 
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
     private void growArray() {
         if (size > array.length * 0.6) {
             int new_size = (int) (array.length * 1.5);
             Object[] new_array = new Object[new_size];
             System.arraycopy(array, 0, new_array, 0, array.length);
             array = new_array;
-            this.length = array.length;
         }
     }
 
@@ -64,7 +101,72 @@ public class NewArrayList<E> {
         return size;
     }
 
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
     public int length() {
-        return length;
+        return array.length;
     }
 }
