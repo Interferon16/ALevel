0063b94e1ec35e94d9cc2dfb1ad8436f271e5d68
Interferon <interfero.16@gmail.com>
2018-05-02 08:42:52 +0300

    update module3

--- moduls/module3/src/main/java/alevel/ExcelAddServlet.java
+++ moduls/module3/src/main/java/alevel/ExcelAddServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @WebServlet("/exceladd")
@@ -14,15 +15,17 @@ public class ExcelAddServlet extends HttpServlet {
     DBManagerExcel dbManagerExcel = new DBManagerExcel();
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String table_name = request.getParameter("table_name");
-        int column_number = Integer.valueOf(request.getParameter("column_number"));
-        String row_data = request.getParameter("row_data");
-
-
-        if(!dbManagerExcel.isTableExist(table_name)){
-            dbManagerExcel.createTable(table_name);
-            dbManagerExcel.addColumns(table_name,column_number,0);
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } finally {
+            reader.close();
         }
+        System.out.println(sb.toString());
 
 
     }
---
+++ moduls/module3/target/module3/scripts/excel.js
@@ -0,0 +1,291 @@
+function openInsert(element_id) {
+    var cell = document.getElementById(element_id);
+    //var tempStyle = cell.getAttribute("style");
+    var temp_width =cell.offsetWidth-4;
+    var temp_height=cell.offsetHeight-6;
+    var tempClass = cell.getAttribute("class");
+    var tempId = cell.getAttribute("id");
+    var tempValue = cell.getAttribute("data-formula");
+    if (!tempValue || tempValue == 0) {
+        tempValue = "";
+    }
+    var newInput = document.createElement("input");
+    //newInput.setAttribute("style", tempStyle);
+    newInput.setAttribute("class", tempClass);
+    newInput.setAttribute("id", tempId);
+    newInput.setAttribute("value", tempValue);
+    newInput.setAttribute("onblur", "getBlur(this.id)");
+    newInput.setAttribute("onkeyup","tapEnter(event,this.id)");
+    newInput.setAttribute("style", "width: "+temp_width+"px; height: "+temp_height+"px");
+    cell.parentNode.insertBefore(newInput, cell);
+    cell.parentNode.removeChild(cell);
+    newInput.focus();
+}
+
+function closeInsert(element_id) {
+    var cell = document.getElementById(element_id);
+    //var tempStyle = cell.getAttribute("style");
+    var tempClass = cell.getAttribute("class");
+    var tempId = cell.getAttribute("id");
+    var tempValue = cell.value;
+    var newCell = document.createElement("td");
+    //newCell.setAttribute("style", tempStyle);
+    newCell.setAttribute("class", tempClass);
+    newCell.setAttribute("id", tempId);
+    if (tempValue.charAt(0) == "+") {
+        tempValue = tempValue.replace("+", "=");
+    }
+    if (!tempValue || tempValue == "0") {
+        newCell.innerText = "";
+    } else {
+        newCell.innerText = parceEquation(tempValue);
+    }
+    newCell.setAttribute("data-formula", tempValue);
+    newCell.setAttribute("ondblclick", "openInsert(this.id)");
+    newCell.setAttribute("onclick","focusAndGetFormula(this.id)");
+    cell.parentNode.insertBefore(newCell, cell);
+    cell.parentNode.removeChild(cell);
+    //setDependentCell(tempValue, cell.id);
+
+}
+
+
+function parceEquation(equatin) {
+    if (equatin.charAt(0) === "=") {
+        var temp_equation = equatin.slice(1);
+        var cellname = /([a-zа-яё]+\d+)/gi;
+
+        temp_equation = temp_equation.replace(cellname, replaceByCellId);
+        //eval error -------------------------
+        try {
+            eval(temp_equation);
+        } catch (e) {
+            return temp_equation;
+        }
+        //eval error -------------------------
+        return eval(temp_equation);
+    }
+
+    else {
+        return equatin;
+    }
+}
+
+function replaceByCellId(match) {
+    try {
+        var a = document.getElementById(match).innerText;
+    }catch (e){
+        a="#ССЫЛКА";
+    }
+    a = a == "" ? 0 : a;
+    return a;
+}
+/*
+function setDependentCell(equation, parrent_id) {
+    var cellname = /([a-zа-яё]+\d+)/gi;
+    var dependencies = equation.match(cellname);
+        for (var i = 0; i < dependencies.length; i++) {
+            element = document.getElementById(dependencies[i]);
+            var temp_dep=[];
+            if(element.getAttribute("data-references")==null){
+                temp_dep=[];
+            }else{
+                temp_dep.push(element.getAttribute("data-references"))
+            }
+            temp_dep.push(parrent_id);
+            element.setAttribute("data-references", temp_dep);
+        }
+
+}
+*/
+
+function addDownRow(cellid) {
+    var cell = document.getElementById(cellid);
+    var parent_row = cell.parentNode;
+    var parent_table =  parent_row.parentNode.parentNode;
+    var new_row = document.createElement("tr");
+    var new_row_num = Number((parent_row.id.split("-"))[1])+1;
+    new_row.setAttribute("class",parent_row.getAttribute("class"));
+    new_row.setAttribute("id",("row-"+new_row_num));
+    var cell_numbers = parent_row.cells.length;
+    for(var i=0;i<cell_numbers;i++){
+        var old_cell=parent_row.cells[i];
+        var new_cell=document.createElement("td");
+/*        var old_cell_class=old_cell.getAttribute("class");
+        if(i==0){
+            new_cell.innerText=new_row_num;
+            new_cell.setAttribute("class","headcell");
+        }else{
+            new_cell.setAttribute("class","cell");
+            new_cell.setAttribute("ondblclick","openInsert(this.id)");
+            new_cell.setAttribute("onclick","focusAndGetFormula(this.id)");
+        }*/
+        new_row.appendChild(new_cell);
+    }
+    insertAfter(new_row,parent_row);
+    refreshRowId(parent_table,parent_row);
+}
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function createButton(inner_text,title) {
+    var button = document.createElement("button");
+    button.setAttribute("type","button");
+    button.setAttribute("class","clickbutton");
+    button.innerText=inner_text;
+    button.setAttribute("title",title);
+    return button;
+}
+
+function refreshRowId(table,parent_row) {
+    var row_number = parent_row.sectionRowIndex;
+    for(var i=row_number;i<table.rows.length;i++){
+        var current_row = table.rows[i];
+        for(var j=0;j<current_row.cells.length;j++){
+            var current_row_number = current_row.sectionRowIndex;
+            var current_cell = current_row.cells[j];
+            if(j==0){
+                current_cell.setAttribute("class","headcell");
+                current_cell.innerText=current_row_number;
+                var plus_button=createButton("+","Добавить строку визу");
+                plus_button.setAttribute("onclick","addDownRow(this.parentNode.id)");
+                var minus_button=createButton("-","Удалить текущую строку");
+                minus_button.setAttribute("onclick","delThisRow(this.parentNode.id)");
+                current_cell.appendChild(plus_button);
+                current_cell.appendChild(minus_button);
+                current_cell.setAttribute("id",current_row_number);
+            }else{
+
+                var current_cell_id=String.fromCharCode(96+j)+current_row_number;
+                current_cell.setAttribute("class","cell");
+                current_cell.setAttribute("id",current_cell_id);
+                current_cell.setAttribute("ondblclick","openInsert(this.id)");
+                current_cell.setAttribute("onclick","focusAndGetFormula(this.id)");
+            }
+        }
+    }
+}
+
+function getBlur(element_id) {
+    closeInsert(element_id);
+    getFormula(element_id);
+}
+
+function tapEnter(key,element_id) {
+    if(key.keyCode == 13) {
+        document.getElementById(element_id).removeAttribute("onblur");
+        closeInsert(element_id);
+        getFormula(element_id);
+
+    }
+}
+
+function focusAndGetFormula(element_id) {
+    try {
+        document.getElementsByClassName("activecell")[0].setAttribute("class", "cell");
+    }catch(e){}
+        document.getElementById(element_id).setAttribute("class", "activecell");
+    getFormula(element_id)
+}
+
+function getFormula(element_id) {
+    var formula = document.getElementById(element_id).getAttribute("data-formula");
+    formula=formula==null?"":formula;
+    document.getElementsByClassName("formulafield")[0].innerText = formula;
+}
+
+function delThisRow(cellid) {
+    var row = document.getElementById(cellid).parentNode;
+        var parent_table=row.parentNode.parentNode;
+    if(parent_table.rows.length>2){
+        var parent_row = row.nextSibling;
+        row.parentNode.removeChild(row);
+        refreshRowId(parent_table,parent_row);
+    }
+}
+
+function addColumn(cell_id) {
+    var cell = document.getElementById(cell_id);
+    var parent_table = cell.parentNode.parentNode.parentNode;
+    var parent_column_number = cell.cellIndex;
+    for(var i=0;i<parent_table.rows.length;i++){
+        var curent_row=parent_table.rows[i];
+        var new_cell=document.createElement("td");
+        if(i==0){
+            new_cell.setAttribute("class","headcell");
+            var temp_id="head-"+(parent_column_number+1);
+            new_cell.setAttribute("id",temp_id);
+        }
+        var parent_cell = curent_row.cells[parent_column_number];
+        insertAfter(new_cell,parent_cell);
+    }
+    var head_row = parent_table.rows[0];
+    var first_row = parent_table.rows[1];
+    refreshRowId(parent_table,first_row);
+    refreshColumnHead(head_row);
+}
+
+function delThisColumn(cell_id) {
+    var cell = document.getElementById(cell_id);
+
+    var parent_table = cell.parentNode.parentNode.parentNode;
+    if(parent_table.rows[0].cells.length>2){
+        var parent_column_number = cell.cellIndex;
+        for(var i=0;i<parent_table.rows.length;i++){
+            var curent_row=parent_table.rows[i];
+            var curent_cell = curent_row.cells[parent_column_number];
+            curent_row.removeChild(curent_cell);
+        }
+        var head_row = parent_table.rows[0];
+        var first_row = parent_table.rows[1];
+        refreshRowId(parent_table,first_row);
+        refreshColumnHead(head_row);
+    }
+}
+
+function refreshColumnHead(head_row) {
+    for(var i=1;i<head_row.cells.length;i++){
+        var column_liter = String.fromCharCode(64+i);
+        head_row.cells[i].innerText=column_liter;
+        var plus_button = createButton("+","Добавить столбец справа");
+        var minus_button = createButton("-","Удалить текущий столбец");
+        plus_button.setAttribute("onclick","addColumn(this.parentNode.id)");
+        minus_button.setAttribute("onclick","delThisColumn(this.parentNode.id)");
+        head_row.cells[i].appendChild(plus_button);
+        head_row.cells[i].appendChild(minus_button);
+    }
+}
+
+function sendJSonToDB() {
+    var curent_table=document.getElementById("maintable");
+    var count=0;
+    var array_of_cells = {};
+    var head={};
+    var table_width=curent_table.rows[0].cells.length-1;
+    var table_height=curent_table.rows.length-1;
+    head["id"]="head";
+    head["data_formula"]="table_size";
+    head["value"]=table_width+"x"+table_height;
+    array_of_cells[++count]=head;
+    //alert(table_width+"+"+table_height);
+    for(var i=1;i<table_height+1;i++){
+        var current_row=curent_table.rows[i];
+        for(var j=1;j<table_width+1;j++){
+            var curent_cell=current_row.cells[j];
+            if(curent_cell.getAttribute("data-formula")!=null){
+                var cell={};
+                cell["id"]=curent_cell.id;
+                cell["data_formula"]=curent_cell.getAttribute("data-formula");
+                cell["value"]=curent_cell.innerText;
+                array_of_cells[++count]=cell;
+            }
+        }
+    }
+    var json = JSON.stringify(array_of_cells);
+    var xhr = new XMLHttpRequest();
+    xhr.open('post',"/exceladd");
+    xhr.send(json);
+    //alert(json);
+}
